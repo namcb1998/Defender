17bc1cb941e5f5e1935473838d5a194f1e668eb3
Linh <yeutrang51@gmail.com>
2018-06-27 12:37:40 +0700

    update

--- app/src/main/java/com/example/namcb1998/defender/MainActivity.java
+++ app/src/main/java/com/example/namcb1998/defender/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.namcb1998.defender;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +16,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.namcb1998.defender.Beans.SOSSource;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,12 +30,15 @@ import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnSOS;
+    private FusedLocationProviderClient mFusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnSOS = findViewById(R.id.btnSOSMainUserActivity);
         btnSOS.setOnClickListener(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Override
@@ -41,9 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId()==R.id.switchMenu){
+        if (item.getItemId() == R.id.switchMenu) {
             goToLoginActivity();
-        }else {
+        } else {
             goToInfoActivity();
         }
         return super.onOptionsItemSelected(item);
@@ -54,18 +63,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void goToLoginActivity() {
-        Intent intent =new Intent(MainActivity.this,LoginActivity.class);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
 
 
     @Override
     public void onClick(View view) {
-        if (view.getId()==R.id.btnSOSMainUserActivity){
+        if (view.getId() == R.id.btnSOSMainUserActivity) {
 
             //need real lat long from gps here
             btnSOS.setEnabled(false);
-            sendSOSSignal(10.832133,106.634282);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    sendSOSSignal(location.getLatitude(),location.getLongitude());
+                }
+            });
         }
     }
 
